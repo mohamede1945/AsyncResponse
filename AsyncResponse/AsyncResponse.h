2d25147a030d04f047f86354cc97c4f25c33e953
Mohamed Afifi <mohamede1945@gmail.com>
2016-07-30 12:19:31 +0200

    Initial Commit

---
+++ AsyncResponse/AsyncResponse.h
@@ -0,0 +1,19 @@
+//
+//  AsyncResponse.h
+//  AsyncResponse
+//
+//  Created by Mohamed Afifi on 7/30/16.
+//  Copyright © 2016 Varaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncResponse.
+FOUNDATION_EXPORT double AsyncResponseVersionNumber;
+
+//! Project version string for AsyncResponse.
+FOUNDATION_EXPORT const unsigned char AsyncResponseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncResponse/PublicHeader.h>
+
+
